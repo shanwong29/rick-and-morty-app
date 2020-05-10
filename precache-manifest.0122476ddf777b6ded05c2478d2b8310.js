a0e201a96051aa760ccaed0d0b6d79a7fc4a65d3
Shan Wong <shan.wong29@gmail.com>
2020-05-10 18:12:40 +0200

    Updates

---
+++ precache-manifest.0122476ddf777b6ded05c2478d2b8310.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f925db463a153528b7ccbaa13415400",
+    "url": "/rick-and-morty-app/index.html"
+  },
+  {
+    "revision": "5f183fe780bb3703e7f3",
+    "url": "/rick-and-morty-app/static/js/2.3e3fa9d8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rick-and-morty-app/static/js/2.3e3fa9d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "649ec35319f92f88822a",
+    "url": "/rick-and-morty-app/static/js/main.d9a532e7.chunk.js"
+  },
+  {
+    "revision": "5dbefe5c6abfa7788989",
+    "url": "/rick-and-morty-app/static/js/runtime-main.323647ac.js"
+  }
+]);
