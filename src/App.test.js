04317f3623ead68e502f017bfafec13ff2474bcb
Shan Wong <shan.wong29@gmail.com>
2021-03-30 03:41:38 +0200

    fix: add test to app

--- src/App.test.js
+++ src/App.test.js
@@ -1,35 +1,48 @@
 import React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
-import { act } from "react-dom/test-utils";
-
 import App from "./App";
-import GlobalStateProvider from "./store/GlobalStateProvider";
 import { allCharData } from "./__mocks__/data/allCharData";
 
-// *****************************    Below: set up for each test    **********************************
-
-const testRender = () => {
-  return render(
-    <GlobalStateProvider>
-      <App />
-    </GlobalStateProvider>
-  );
-};
-
-// ************************************************************************************************
-describe("Rendering", () => {
-  it("should fetch `mockCharData` and show the first 10 data", async () => {
-    testRender();
-
+describe("Inital render when api response is successful", () => {
+  beforeEach(async () => {
+    render(<App />);
     const firstDisplayCharName = allCharData[0].name;
-    const lastDisplayCharName = allCharData[9].name;
 
     await waitFor(() =>
       expect(screen.getByAltText(`${firstDisplayCharName}`)).toBeInTheDocument()
     );
+  });
 
-    screen.debug();
+  it("should show the first 10 data", () => {
+    const lastDisplayCharName = allCharData[9].name;
 
     expect(screen.getByAltText(`${lastDisplayCharName}`)).toBeInTheDocument();
+  });
+
+  it("should show a name input field, a species input field, a status select field and a clear button ", async () => {
+    expect(
+      screen.getByLabelText(/name/i, { selector: "input" })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByLabelText(/species/i, { selector: "input" })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByLabelText(/status/i, { selector: "select" })
+    ).toBeInTheDocument();
+
+    const clearBtn = screen.getByText("CLEAR", { selector: "button" });
+    expect(clearBtn).toBeInTheDocument();
+  });
+
+  it("should show page control for the first 10 pages, and the last page", () => {
+    for (let i = 1; i <= 10; i++) {
+      const pageBtn = screen.getByText(i, { selector: "button" });
+      expect(pageBtn).toBeInTheDocument();
+    }
+
+    const lastPageBtn = screen.getByText(Math.ceil(allCharData.length / 10), {
+      selector: "button",
+    });
+    expect(lastPageBtn).toBeInTheDocument();
   });
 });
