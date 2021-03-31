be0c3f59e67094c4280a450e69bc6b2fc2fb2748
shanwong29 <shanwong29@users.noreply.github.com>
2021-03-31 15:31:08 +0000

    deploy: 15111c8cb14903a20f4c36d70af2ef7b8620099a

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rick-and-morty-app/precache-manifest.5ac09836491f703135043ec44848ed64.js"
+  "/rick-and-morty-app/precache-manifest.e73648abc66cb5ddf036eac43b296a1b.js"
 );
 
 self.addEventListener('message', (event) => {
